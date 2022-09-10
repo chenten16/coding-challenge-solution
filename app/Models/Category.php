d66ea8eb39cd4f64934819954aa7ebd94525d76d
chenten16 <mfilali455@gmail.com>
2022-09-10 20:11:58 +0100

    assigning relationships for models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    //relationship for parent category
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Category', 'parent_id');
+    }
+    //relationship for getting subcategories
+    public function subCategories()
+    {
+        return $this->hasMany('App\Models\Category', 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->belongstoMany('App\Models\Product');
+    }
 }
